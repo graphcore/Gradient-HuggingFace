e9e8b5c1276b579534dc41e0c2ae01ef90fc875b
Johannes Roux <johannesr@graphcore.ai>
2023-08-16 12:24:19 +0000

    remove target files to allow yml to copy them over

--- multimodal/magma/popxl/modelling/clip_resnet/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2023 Graphcore Ltd. All rights reserved.
-
-from .batch_norm import *
-from .bottleneck import *
-from .stem import *
-from .modified_resnet import *
-from .attention_pool import *
--- multimodal/magma/popxl/modelling/gptj/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023 Graphcore Ltd. All rights reserved.
-
-from .attention import *
-from .decoder import *
-from .embedding import *
-from .feed_forward import *
-from .gptj_lm import *
-from .gptj_model import *
-from .finetuneanon_mapping import *
